84c139b35404323fa60ec499d177a045505ff4cc
Jm <jay.delasalas0211@gmail.com>
2022-10-08 10:04:00 +0800

    Updated assets

---
+++ header.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<title><?php echo $_SESSION['setting_hotel_name'] ?></title>
+<!-- Favicon-->
+<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+
+<!-- Bootstrap -->
+<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<!-- Fontawesome -->
+<link href="libfontawesome/css/all.min.css" rel="stylesheet" />
