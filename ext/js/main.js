3adbf531ce2b270c9d52dc786d59a2b89b658f66
Jerome <jerome.marketingcrossmedia@gmail.com>
2022-10-17 17:53:51 +0800

    make graph working

--- ext/js/main.js
+++ ext/js/main.js
@@ -1,38 +1,4 @@
 
-if($("#booked_graph")) {
-    Highcharts.chart('booked_graph', {
-        chart: {
-            type: 'line'
-        },
-        title: {
-            text: 'Monthly Client Booked'
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Total Booked'
-            }
-        },
-        plotOptions: {
-            line: {
-                dataLabels: {
-                    enabled: true
-                },
-                enableMouseTracking: false
-            }
-        },
-        series: [{
-            name: 'Lemery',
-            data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'Calatagan',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }]
-    });
-}
-
 $(".btn-edit-customer").click(function() {
     let id = $(this).data('id')
 
