ba88758904f12046967dd71a2ee23e546a8c3e64
Jerome Suyat <48372200+lhabzter21@users.noreply.github.com>
2022-10-17 17:54:31 +0800

    make graph working (#22)
    Co-authored-by: Jerome <jerome.marketingcrossmedia@gmail.com>

--- admin/index.php
+++ admin/index.php
@@ -29,14 +29,55 @@
   <?php include('modules/products_offer.php'); ?>
   <?php include('modules/users.php'); ?>
   <?php include('modules/site_settings.php'); ?>
+
+  <?php
+    $months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    $num_users = [];
+    foreach($months as $index => $month) {
+      $m = date('Y-m', strtotime(date('Y').'-'.$index));
+      $_customers = $conn->query("SELECT count(*) as total_user from customers WHERE deleted_at IS NULL AND DATE_FORMAT(created_at, '%Y-%m') = '$m' GROUP BY DATE_FORMAT(created_at, '%Y-%m');");
+      $num_users[] = $_customers->num_rows > 0 ? intVal($_customers->fetch_array()[0]):0;
+    }
+?>
 </main>
 
 <?php include('components/modal.php');?>
 
 </body>
 
-
 <?php include('components/footer.php');?>
+<script>
+  if($("#booked_graph")) {
+    Highcharts.chart('booked_graph', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Monthly Client Registered'
+        },
+        xAxis: {
+            categories: <?php echo json_encode($months)?>
+        },
+        yAxis: {
+            title: {
+                text: 'Total Registered'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Customers',
+            data: <?php echo json_encode($num_users)?>
+        }]
+    });
+  }
+</script>
 <script src="../ext/js/main.js"></script>
 
 </html>
